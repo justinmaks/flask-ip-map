31ece8eddb728e07fac64575367d56e2b87fb9bc
justinmaks79@gmail.com <justinmaks79@gmail.com>
2024-06-14 17:47:55 -0400

    db script

--- create_db.py
+++ create_db.py
@@ -2,9 +2,20 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
+
+# Check if the visits table exists
 c.execute('''
-          CREATE TABLE visits
-          (id INTEGER PRIMARY KEY, ip TEXT, latitude REAL, longitude REAL)
-          ''')
+    SELECT name FROM sqlite_master WHERE type='table' AND name='visits';
+''')
+
+table_exists = c.fetchone()
+
+# If the visits table does not exist, create it
+if not table_exists:
+    c.execute('''
+        CREATE TABLE visits
+        (id INTEGER PRIMARY KEY, ip TEXT, latitude REAL, longitude REAL)
+    ''')
+
 conn.commit()
 conn.close()
