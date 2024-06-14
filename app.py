004464292fcb566a67bc7172c6f7b0790529e062
justinmaks79@gmail.com <justinmaks79@gmail.com>
2024-06-14 15:24:25 -0400

    first ip

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def query_db(query, args=(), one=False):
 
 def get_client_ip():
     if request.headers.getlist("X-Forwarded-For"):
-        ip = request.headers.getlist("X-Forwarded-For")[0]
+        ip = request.headers.getlist("X-Forwarded-For")[0].split(',')[0].strip()
     elif request.headers.get("CF-Connecting-IP"):
         ip = request.headers.get("CF-Connecting-IP")
     else:
